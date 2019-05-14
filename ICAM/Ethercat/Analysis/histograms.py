369393bd7decde69a14353c2c799d2e7a297747b
GioshTandoi <mariagio.tandoi@gmail.com>
2019-05-14 14:42:22 +0200

    Calculating distributions of the spread depending on where tha machine is moving (along Y, X or Z axis)

--- ICAM/Ethercat/Analysis/histograms.py
+++ ICAM/Ethercat/Analysis/histograms.py
@@ -1,4 +1,40 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'C:\Users\exprivia\Desktop\DF\ICAM\Dati_Rielaborati\MasterData\chunks\ethercat_icam_v7_chunk1_events.csv')
+chunk_n = 9
+df = pd.read_csv(r'C:\Users\exprivia\Desktop\DF\ICAM\Dati_Rielaborati\MasterData\chunks\ethercat_icam_v7_chunk'+str(chunk_n)+'_events.csv')
 df.describe()
+
+spread1 = (np.array(df['yactualposition'].values) - np.array(df['ypositionfeedback1value'].values)).reshape(len(df['ypositionfeedback1value'].values), 1)
+ZFrequencyMonitor = np.array(df['zfrequencymonitor'].values).reshape(len(df['zfrequencymonitor'].values), 1)
+YVelocityFeedbackValue = np.array(df['yvelocityfeedbackvalue'].values).reshape(len(df['yvelocityfeedbackvalue'].values),1)
+XVelocityFeedbackValue = np.array(df['xvelocityfeedbackvalue'].values).reshape(len(df['yvelocityfeedbackvalue'].values), 1)
+
+new_df = pd.DataFrame(data=np.concatenate((spread1, ZFrequencyMonitor, YVelocityFeedbackValue, XVelocityFeedbackValue), axis=1),
+                      columns=['spread1', 'zfrequencymonitor', 'yvelocityfeedbackvalue', 'xvelocityfeedbackvalue'])
+
+is_moving_Z = new_df['zfrequencymonitor'] != 0
+is_moving_Y = new_df['yvelocityfeedbackvalue'] != 0
+is_moving_X = new_df['xvelocityfeedbackvalue'] != 0
+
+new_df_Z = new_df[is_moving_Z]
+new_df_Y = new_df[is_moving_Y]
+new_df_X = new_df[is_moving_X]
+
+
+"""n, bins, patches = plt.hist(x=new_df_Z['spread1'], bins='auto', color='#0504aa',
+                            alpha=0.7, rwidth=0.85)"""
+plt.figure(chunk_n)
+plt.hist(abs(new_df_Z['spread1']), bins='auto', alpha=0.5, label='moving_in_Z')
+plt.hist(abs(new_df_Y['spread1']), bins='auto', alpha=0.5, label='moving_in_Y')
+plt.hist(abs(new_df_X['spread1']), bins='auto', alpha=0.5, label='moving_in_X')
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('Spread1 (10^-4 mm)')
+plt.ylabel('Frequency')
+plt.title('Abs Spread1 Histogram (chunk '+str(chunk_n)+')')
+plt.legend(loc='upper right')
+plt.show()
+#maxfreq = n.max()
+# Set a clean upper y-axis limit.
+#plt.ylim(ymax=np.ceil(maxfreq / 10) * 10.5 if maxfreq % 10 else maxfreq + 20)
